77124aa3384c726a444a7daf615c4bcbab79f58c
kylehek <kylehek99@gmail.com>
2024-01-22 10:37:18 +0000

    removing default / route to fix issue

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -160,7 +160,7 @@ function App() {
         )}
         <Banner />
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/" element={<ToursSection />} />
           <Route path="/home" element={<ToursSection />} />
           <Route path="/store" element={
             <>
